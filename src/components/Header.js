947d8363c2b1d34ae30b6f19d183d872a1bd83bc
Hannannawaw1 <hnawawi939@gmail.com>
2022-09-26 10:53:53 +0700

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 // atau langsung tambahkan dengan sx={{}}
-import React from 'react';
+import React from "react";
+import "./Header.css";
 
 // Header berisi judul dan subtitle
 // Judul: Call a Friend
 // Subtitle: Your friendly contact app
 const Header = () => {
   return (
-    <></>
+    <>
+      <h1>Call a Friend</h1>
+      <p className="subtitle">
+        <span>Your friendly contact app</span>
+      </p>
+    </>
   );
 };
 
